fd8ab7235a0923c8e1a25129a1a89a9dc26bd07f
Beasley <63673533+beasleyDOTcom@users.noreply.github.com>
2020-06-22 09:18:09 -0700

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
